dc640f209966a975f8d55c7878a223b255a0d4e0
tgshaffa-stack <tgshaffa@gmail.com>
2026-08-05 23:05:33 +0700

    Update server IP to home network 192.168.0.38

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -1,1 +1,1 @@
-package org.telegram.messenger; public class BuildVars { public static boolean DEBUG_VERSION = true; public static boolean USE_SERVER_ALL = true; public static String BUILD_IP = "10.137.114.169"; }
+package org.telegram.messenger; public class BuildVars { public static boolean DEBUG_VERSION = true; public static boolean USE_SERVER_ALL = true; public static String BUILD_IP = "192.168.0.38"; }
